ff9037b7389739bba5e530a2c2a64930a8268015
Levit Osei Kanner <lkanner21@gmail.com>
2019-10-31 21:08:39 +0000

    Adds validations

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -10,9 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     //Mark:- Properties
-   @State private var Usedwords = [String]()
+   @State private var usedWords = [String]()
    @State private var rootWord = ""
    @State private var newWord = ""
+   @State private var showingAlert = false
+   @State private var alertMessage = ""
+   @State private var alertTitle = ""
+    
     
     //Mark:- Body
     var body: some View {
@@ -25,13 +29,16 @@ struct ContentView: View {
                 .padding()
                 
                 
-                List(Usedwords, id: \.self){
+                List(usedWords, id: \.self){
                     Text("\($0)")
                     Image(systemName: "\($0.count).circle.fill")
                 }
             }
             .navigationBarTitle("\(rootWord)")
             .onAppear(perform: startGame)
+            .alert(isPresented: $showingAlert) { () -> Alert in
+                Alert(title: Text(alertTitle), message: Text(alertMessage), dismissButton: .default(Text("Okay")))
+            }
         }
     }
     
@@ -40,9 +47,24 @@ struct ContentView: View {
     func addNewWord(){
         let new = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
         guard new.count > 0 else { return }
-        Usedwords.insert(new, at: 0)
-        newWord = ""
+       
         
+        guard isOriginal(word: new) else {
+            configureAlert(title: "Word used already ", message: "Words can't be repeated")
+            return
+        }
+        guard isPossible(word: new) else {
+            configureAlert(title: "Word invalid", message: "Words must be from the above")
+            return
+        }
+        
+        guard isAWord(word: new) else {
+            configureAlert(title: "Word doesn't exit", message: "Word invalid")
+            return
+        }
+        
+        usedWords.insert(new, at: 0)
+        newWord = ""
     }
     
     func startGame(){
@@ -54,6 +76,38 @@ struct ContentView: View {
             }
         }
         fatalError("Couldn't load start.txt from Bundle")
+    }
+    
+    //Validation
+    func isOriginal(word: String) -> Bool {
+        !usedWords.contains(word)
+    }
+    
+    func isPossible(word: String) -> Bool{
+        var temp = rootWord
+        
+        for letter in word {
+            if let position = temp.firstIndex(of: letter){
+                temp.remove(at: position)
+            }else{
+                return false
+            }
+        }
+        return true
+    }
+    
+    
+    func isAWord(word: String) -> Bool{
+        let checker = UITextChecker()
+        let range = NSRange(location: 0, length: word.utf16.count)
+        let misSpelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+        return misSpelledRange.location == NSNotFound
+    }
+    
+    func configureAlert(title: String , message: String){
+        alertTitle = title
+        alertMessage = message
+        showingAlert = true
     }
 }
 
